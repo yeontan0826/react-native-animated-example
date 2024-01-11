7093b3d73826c133bd0aff40b9e39dea4ec2232e
yeontan0826 <yeontan0826@gmail.com>
2024-01-11 16:17:50 +0900

    상호작용 감지 로직 추가

--- src/screens/panResponderFontSlider/index.tsx
+++ src/screens/panResponderFontSlider/index.tsx
@@ -1,33 +1,67 @@
-import {useState} from 'react';
-import {Text, TouchableWithoutFeedback, View} from 'react-native';
+import {useRef, useState} from 'react';
+import {
+  Animated,
+  PanResponder,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+
+const BOX = 50;
+const CIRCLE = 24;
+const FONT = [
+  {
+    title: {fontSize: 20, lineHeight: 32},
+    body: {fontSize: 12},
+  },
+  {
+    title: {fontSize: 24, lineHeight: 38},
+    body: {fontSize: 14},
+  },
+  {
+    title: {fontSize: 30, lineHeight: 40},
+    body: {fontSize: 16},
+  },
+  {
+    title: {fontSize: 36, lineHeight: 50},
+    body: {fontSize: 18},
+  },
+];
 
 const PanResponderFontSliderScreen = (): JSX.Element => {
   const [step, setStep] = useState(0);
+  const circleAnim = useRef(new Animated.Value(0)).current;
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onMoveShouldSetPanResponder: () => true,
+    onPanResponderStart: () => {
+      circleAnim.setValue(step * BOX);
+    },
+    onPanResponderMove: (_, gestureState) => {
+      circleAnim.setValue(gestureState.dx + step * BOX);
+    },
+    onPanResponderEnd: (_, gestureState) => {
+      const fontStep = step + Math.round(gestureState.dx / 50);
+      const toValue = fontStep * BOX;
+      setStep(fontStep);
+
+      Animated.spring(circleAnim, {
+        toValue,
+        speed: 100,
+        useNativeDriver: true,
+      }).start();
+    },
+  });
 
   const onPress = (index: number) => {
     setStep(index);
+    Animated.spring(circleAnim, {
+      toValue: index * BOX,
+      speed: 100,
+      useNativeDriver: true,
+    }).start();
   };
-
-  const BOX = 50;
-  const CIRCLE = 24;
-  const FONT = [
-    {
-      title: {fontSize: 20, lineHeight: 32},
-      body: {fontSize: 12},
-    },
-    {
-      title: {fontSize: 24, lineHeight: 38},
-      body: {fontSize: 14},
-    },
-    {
-      title: {fontSize: 30, lineHeight: 40},
-      body: {fontSize: 16},
-    },
-    {
-      title: {fontSize: 38, lineHeight: 50},
-      body: {fontSize: 18},
-    },
-  ];
 
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
@@ -68,6 +102,7 @@ const PanResponderFontSliderScreen = (): JSX.Element => {
                 onPress={() => onPress(index)}>
                 <View
                   style={{
+                    borderWidth: 1,
                     width: BOX,
                     height: BOX,
                     justifyContent: 'center',
@@ -85,14 +120,16 @@ const PanResponderFontSliderScreen = (): JSX.Element => {
               </TouchableWithoutFeedback>
             ))}
           </View>
-          <View
+          <Animated.View
+            {...panResponder.panHandlers}
             style={{
               position: 'absolute',
-              left: BOX / 2 - CIRCLE / 2 + step * BOX,
+              left: BOX / 2 - CIRCLE / 2,
               width: CIRCLE,
               height: CIRCLE,
               backgroundColor: '#333',
               borderRadius: CIRCLE / 2,
+              transform: [{translateX: circleAnim}],
             }}
           />
         </View>
